e44194bd767df887c0f958f20682ad1361f24def
Recep Sami <resake5150@gmail.com>
2023-11-29 14:05:25 +0300

    mobile trying

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,189 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.VFX;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance;
+
+    public PlayerMovement player;
+    public TextMeshProUGUI levelCompleted;
+    public TextMeshProUGUI level;
+
+    public ParticleSystem vfx1;
+    public ParticleSystem vfx2;
+    public GameObject panel;
+    public GameObject levelGrid;
+    public GameObject gameOver;
+    // public GameObject playerPrefab;
+
+
+    public int amountOfFloor;
+    // float startDelay = 0;
+    //float intervalDelay = 2f;
+    public bool isLevelCompleted;
+    public bool isButtonClicked;
+    bool ischecked;
+    int index = 1;
+
+    public Button[] button;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+
+
+        if (Instance != null)
+        {
+
+            Destroy(gameObject);
+
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
+
+
+        isButtonClicked = false;
+
+        level.text = "Level " + (SceneManager.GetActiveScene().buildIndex + 1).ToString();
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Level Locekd-unlocked system
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        for (int i = 0; i < button.Length; i++)
+        {
+            button[i].interactable = false;
+        }
+        for (int i = 0; i < unlockedLevel; i++)
+        {
+            button[i].interactable = true;
+        }
+
+        //Check If The Painteed Floor and The Amount Of The Floors Matched If So Display "Level Completed" UI...
+        if (player.countPaintedFloor == amountOfFloor && isLevelCompleted == false)
+        {
+            levelCompleted.gameObject.SetActive(true);
+            isLevelCompleted = true;
+            if(SceneManager.GetActiveScene().buildIndex == 9)
+            {
+                gameOver.gameObject.SetActive(true);
+            }
+            gameOver.gameObject.SetActive(false);
+
+            Confetti();
+            StartCoroutine(countDown());
+
+            if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+            {
+                PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+                PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+                PlayerPrefs.SetInt("Index", index);
+                PlayerPrefs.Save();
+
+            }
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+    void Confetti()
+    {
+        if (isLevelCompleted == true)
+        {
+            vfx1.Play();
+            vfx2.Play();
+        }
+
+    }
+
+    //Start The Game
+    public void TapToCountinue()
+    {
+        panel.gameObject.SetActive(false);
+        isButtonClicked = true;
+    }
+
+
+    public void HandleInputData(int val)
+    {
+        SceneManager.LoadScene(val);
+        levelGrid.SetActive(false);
+        if (val == 0)
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+
+    public void LoadLevelGrid()
+    {
+        levelGrid.gameObject.SetActive(true);
+    }
+
+    public void BackFromGrid()
+    {
+        levelGrid.gameObject.SetActive(false);
+    }
+
+    public void ResetLevels()
+    {
+
+
+        PlayerPrefs.DeleteAll();
+
+    }
+
+
+
+
+
+
+
+
+
+    IEnumerator countDown()
+    {
+        yield return new WaitForSeconds(1);
+
+
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        level.text = "Level " + (SceneManager.GetActiveScene().buildIndex + 2).ToString();
+
+        isLevelCompleted = false;
+        panel.gameObject.SetActive(true);
+        levelCompleted.gameObject.SetActive(false);
+        isButtonClicked = false;
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,338 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 10;
+
+
+    RaycastHit wall;
+    Rigidbody playerRb;
+
+    public GameObject player;
+    Color color;
+    Color playerColor;
+
+    Vector2 firstPosition;
+    Vector2 lastPosition;
+    Vector2 moveDirection;
+
+
+
+    bool isMovementForward;
+    bool isVerticalMovementChecked;
+    bool isHorizontalMovementChecked;
+    bool doubleClicked;
+    bool isMouseClicked;
+
+
+    public int countPaintedFloor;
+
+    public Button[] button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        GameManager.Instance.player = GetComponent<PlayerMovement>();
+
+        SetamountOfFloor();
+
+        //Setting a random color for player...
+        color = new Color(r: UnityEngine.Random.Range(0F, 1F), g: UnityEngine.Random.Range(0F, 1F), b: UnityEngine.Random.Range(0F, 1F));
+        player.GetComponent<Renderer>().material.color = color;
+        playerColor = player.GetComponent<Renderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Control the player with mouse inputs
+        //MouseController();
+        Movement();
+
+
+    }
+
+
+    public void SetamountOfFloor()
+    {
+        GameManager.Instance.amountOfFloor = GameObject.FindGameObjectsWithTag("Floor").Length;
+
+    }
+
+
+
+    //Paint the Floor And Keep Track Of Painted Floors
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.GetComponent<MeshRenderer>().material.color != playerColor && other.gameObject.CompareTag("Floor"))
+    //    {
+
+    //        other.gameObject.GetComponent<MeshRenderer>().material.color = playerColor;
+    //        countPaintedFloor++;
+
+
+    //    }
+
+    //}
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetComponent<MeshRenderer>().material.color != playerColor && other.gameObject.CompareTag("Floor"))
+        {
+
+            other.gameObject.GetComponent<MeshRenderer>().material.color = playerColor;
+            countPaintedFloor++;
+
+
+        }
+    }
+
+
+
+
+
+    //void MouseController()
+    //{
+    //    if (GameManager.Instance.isButtonClicked == true && GameManager.Instance.isLevelCompleted == false)
+    //    {
+
+    //        if (Input.GetMouseButtonDown(0) && doubleClicked == false)
+    //        {
+    //            firstPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    //            isMouseClicked = true;
+    //            doubleClicked = true;
+
+    //        }
+    //        else if (Input.GetMouseButtonUp(0))
+    //        {
+    //            isMouseClicked = false;
+    //        }
+
+    //        if (isMouseClicked == true)
+    //        {
+    //            lastPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+
+    //            moveDirection = new Vector2(lastPosition.x - firstPosition.x, lastPosition.y - firstPosition.y).normalized;
+
+    //            isMovementForward = true;
+
+
+
+    //        }
+
+
+
+
+    //        if (isVerticalMovementChecked == false)
+    //        {
+
+    //            if (moveDirection.x > 0 && moveDirection.y > -0.5f && moveDirection.y < 0.5f)
+    //            {
+    //                if (Physics.Raycast(transform.position, transform.right, out wall, 0.5f))
+    //                {
+    //                    if (wall.collider.gameObject.tag == "Wall")
+    //                    {
+    //                        isMovementForward = false;
+    //                        isHorizontalMovementChecked = false;
+    //                        doubleClicked = false;
+
+    //                    }
+
+
+    //                }
+    //                if (isMovementForward == true)
+    //                {
+    //                    transform.Translate(Vector3.right * speed * Time.deltaTime);
+    //                    isHorizontalMovementChecked = true;
+    //                }
+    //            }
+    //        }
+
+    //        if (isVerticalMovementChecked == false)
+    //        {
+
+    //            if (moveDirection.x < 0 && moveDirection.y > -0.5f && moveDirection.y < 0.5f)
+    //            {
+    //                if (Physics.Raycast(transform.position, -transform.right, out wall, 0.5f))
+    //                {
+    //                    if (wall.collider.gameObject.tag == "Wall")
+    //                    {
+    //                        isMovementForward = false;
+    //                        isHorizontalMovementChecked = false;
+    //                        doubleClicked = false;
+    //                    }
+
+
+    //                }
+    //                if (isMovementForward == true)
+    //                {
+    //                    isHorizontalMovementChecked = true;
+    //                    transform.Translate(Vector3.left * speed * Time.deltaTime);
+    //                }
+    //            }
+    //        }
+
+    //        if (isHorizontalMovementChecked == false)
+    //        {
+
+    //            if (moveDirection.y > 0 && moveDirection.x > -0.5f && moveDirection.x < 0.5f)
+    //            {
+    //                if (Physics.Raycast(transform.position, transform.forward, out wall, 0.5f))
+    //                {
+    //                    if (wall.collider.gameObject.tag == "Wall")
+    //                    {
+    //                        isMovementForward = false;
+    //                        isVerticalMovementChecked = false;
+    //                        doubleClicked = false;
+    //                        Debug.Log("Checked");
+    //                    }
+
+
+    //                }
+    //                if (isMovementForward == true)
+    //                {
+    //                    isVerticalMovementChecked = true;
+    //                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    //                }
+    //            }
+    //        }
+    //        if (isHorizontalMovementChecked == false)
+    //        {
+
+    //            if (moveDirection.y < 0 && moveDirection.x > -0.5f && moveDirection.x < 0.5f)
+    //            {
+    //                if (Physics.Raycast(transform.position, -transform.forward, out wall, 0.5f))
+    //                {
+    //                    if (wall.collider.gameObject.tag == "Wall")
+    //                    {
+    //                        isMovementForward = false;
+    //                        isVerticalMovementChecked = false;
+    //                        doubleClicked = false;
+    //                    }
+
+
+    //                }
+    //                if (isMovementForward == true)
+    //                {
+    //                    isVerticalMovementChecked = true;
+    //                    transform.Translate(Vector3.back * speed * Time.deltaTime);
+    //                }
+    //            }
+
+    //        }
+    //    }
+
+    //}
+    Vector3 currentDirection;
+    private void Movement()
+    {
+        if (GameManager.Instance.isButtonClicked == true && GameManager.Instance.isLevelCompleted == false)
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                firstPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                isMouseClicked = true;
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                lastPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+                moveDirection = new Vector2(lastPosition.x - firstPosition.x, lastPosition.y - firstPosition.y).normalized;
+
+                if (!isMovementForward && (MathF.Abs(moveDirection.y) > 0.75f || Mathf.Abs(moveDirection.x) > 0.75f))
+                {
+                    isMovementForward = true;
+
+                    if (isVerticalMovementChecked == false)
+                    {
+                        if (moveDirection.x > 0 && moveDirection.y > -0.5f && moveDirection.y < 0.5f)
+                        {
+                            currentDirection = transform.right;
+                        }
+                        else if (moveDirection.x < 0 && moveDirection.y > -0.5f && moveDirection.y < 0.5f)
+                        {
+                            currentDirection = -transform.right;
+                        }
+                    }
+                    if (isHorizontalMovementChecked == false)
+                    {
+                        if (moveDirection.y > 0 && moveDirection.x > -0.5f && moveDirection.x < 0.5f)
+                        {
+                            currentDirection = transform.forward;
+                        }
+                        else if (moveDirection.y < 0 && moveDirection.x > -0.5f && moveDirection.x < 0.5f)
+                        {
+                            currentDirection = -transform.forward;
+                        }
+                    }
+                }
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                isMouseClicked = false;
+            }
+
+
+            if (isMovementForward == true)
+            {
+                playerRb.velocity = currentDirection * speed;
+
+                if (currentDirection.x != 0)
+                {
+                    isHorizontalMovementChecked = true;
+                }
+                else if (currentDirection.y != 0)
+                {
+                    isVerticalMovementChecked = true;
+                }
+
+                if (Physics.Raycast(transform.position, currentDirection, out wall, 0.45f))
+                {
+                    if (wall.collider.gameObject.tag == "Wall")
+                    {
+                        
+                        isMovementForward = false;
+                        isVerticalMovementChecked = false;
+                        isHorizontalMovementChecked = false;
+
+                    }
+
+
+
+                }
+
+            }
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
